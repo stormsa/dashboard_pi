123a756289cad3a4c9ba17b64247865801215d98
stormsa <sofiane.aziri@gmail.com>
2018-03-11 15:10:18 +0100

    Add Carousel interfaces

--- src/Components/Ratp.js
+++ src/Components/Ratp.js
@@ -11,7 +11,12 @@ class Ratp extends Component {
         this.state = {
             title: "Chargement en cours",
             message: "",
-            schedules: []
+            slug: "",
+            colorTraffic: "green",
+            schedules: [],
+            divStyle: {
+                backgroundColor: 'green'
+            }
         }
         this.getTraffic = this.getTraffic.bind(this)
         this.getHoraire = this.getHoraire.bind(this)
@@ -28,23 +33,35 @@ class Ratp extends Component {
     getTraffic() {
         fetch(API + DATA_TYPES[1]+"/"+ this.transportType + "/"+this.line)
             .then(response => response.json())
-            .then(data => this.setState({
-                title: data.result.title,
-                message: data.result.message
+            .then(data => {
+                this.setState({
+                    title: data.result.title,
+                    message: data.result.message,
+                    slug: data.result.slug
+                })
+
+                if (this.state.slug !== "normal") {
+                    this.setState({
+                        divStyle: {
+                            backgroundColor: 'green'
+                        }
+                    })
+                }
             })
-            )
     }
     getHoraire() {
         fetch(API + DATA_TYPES[0] + "/" + this.transportType + "/" + this.line + "/" + this.station + "/" + this.way)
             .then(response => response.json())
             .then(data => {
-                console.log(data.result.schedules)
                 this.setState({
                     schedules: data.result.schedules
                 })
 
             })
 
+    }
+    componentDidUpdate(props){
+        console.log("component update")
     }
   render() {
       const eachSchedule = function(schedule){
@@ -55,16 +72,37 @@ class Ratp extends Component {
           )
       }
       const Schedule = function(props){
+          let time = props.message.split(":")
+
+          let timeBT = diff_minutes(time);
           return (
-              <div>
-                Prochain train : {props.destination} {props.message}
+              <div className="row">
+                  <div className="col-md-6 col-6">{props.destination}</div>
+                  <div className="col-md-6 col-6">{props.message} ({timeBT} mn) </div>
               </div>
           )
       }
+       const diff_minutes = function(time)
+      {
+          let date = new Date;
+          let minutes = date.getMinutes();
+          let hour = date.getHours();
+          while(parseInt(time[0]) > parseInt(hour)){
+              time[1] = parseInt(time[1]) + 60;
+              hour++;
+          }
+          if(parseInt(time[1])> parseInt(minutes)){
+              return (time[1] - minutes)
+          }
+      }
     return (
-      <div className="RATP col-md-4">
-          <span>{this.line} {this.station} : {this.state.title}</span>
-          <span> {this.state.message}</span>
+      <div className="APP RATP">
+          <div style={this.state.divStyle} className="row"><b>{this.line} {this.station} </b> <button className="fa fa-refresh"> </button></div>
+          <span className="row"> {this.state.slug !== "normal" ? this.state.message : ""}</span>
+          <div className="row">
+              <div className="col-md-6 col-6">Destination</div>
+              <div className="col-md-6 col-6">Temps</div>
+          </div>
           <div>
               {this.state.schedules.map(eachSchedule)}
           </div>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
 import './Dashboard.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-responsive-carousel/lib/styles/carousel.css';
 import Ratp from "./Components/Ratp";
+import {Carousel} from 'react-responsive-carousel';
 
 // RATP CONSTANTES
 const TRANSPORT_TYPE = ["rers", "metro"]
@@ -14,15 +18,36 @@ class Dashboard extends Component {
     }
   render() {
     return (
-      <div className="Dashboard container">
-          <div className="row">
-              <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Le+Parc+De+Saint+Maur" way="A"/>
-              <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Auber" way="A"/>
-          </div>
+        <div className="container">
+            <Carousel showArrows={true} onClickItem={console.log("hello")}>
+                <div>
+                    <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Le+Parc+De+Saint+Maur" way="A"/>
+                    <p className="legend">RATP APP</p>
+                </div>
+                <div>
+                    <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Auber" way="A"/>
+                    <p className="legend">RATP APP</p>
+                </div>
+                <div>
+                    <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Saint+Maur+Creteil" way="A"/>
+                    <p className="legend">RATP APP</p>
+                </div>
+                <div>
+                    <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Le+Parc+De+Saint+Maur" way="A"/>
+                    <p className="legend">RATP APP</p>
+                </div>
+                <div>
+                    <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Le+Parc+De+Saint+Maur" way="A"/>
+                    <p className="legend">RATP APP</p>
+                </div>
+                <div>
+                    <Ratp transportType={TRANSPORT_TYPE[0]} line={LINES[0]} station="Le+Parc+De+Saint+Maur" way="A"/>
+                    <p className="legend">RATP APP</p>
+                </div>
+            </Carousel>
+        </div>
 
-      </div>
     );
   }
 }
-
-export default Dashboard;
+export default Dashboard
