0b383adfddc630b2a695d027e3c1be59b40ac426
Sofiane <sofiane.aziri@carboatmedia.fr>
2019-01-12 15:57:31 +0100

    Add yeelight autodiscover

--- server.py
+++ server.py
@@ -4,13 +4,24 @@ from flask import (
     Response,
     json
 )
-from yeelight import Bulb
-bulb = Bulb("192.168.1.32")
-#import bulb YeelightWifiBulbLanCtrl.py
+
+from yeelight import (
+    discover_bulbs,
+    Bulb
+)
+try:
+    bulb_ip = discover_bulbs()[0]['ip']
+    bulb = Bulb(bulb_ip)
+except Exception as err:
+      print("Oops!  We can't find a yeelight bulb ip", err)
 
 # Create the application instance
 app = Flask(__name__, static_folder="dist", template_folder="public")
+
+
 # Create a URL route in our application for "/"
+
+
 @app.route('/')
 def home():
     """
@@ -21,27 +32,32 @@ def home():
     """
     return render_template('index.html')
 
+
 # Create a URL route in our application for "/"
 @app.route('/getState')
 def getState():
-	state = bulb.get_properties()
-	return Response(json.dumps(state), status=200, mimetype='application/json')
+    state = bulb.get_properties()
+    return Response(json.dumps(state), status=200, mimetype='application/json')
+
 
 @app.route('/turnOn')
 def turnOn():
     bulb.turn_on()
     return Response("{'power':'on'}", status=200, mimetype='application/json')
 
+
 @app.route('/turnOff')
 def turnOff():
     bulb.turn_off()
     return Response("{'power':'off'}", status=200, mimetype='application/json')
+
 
 @app.route('/toggle')
 def toggle():
     bulb.toggle()
     state = bulb.get_properties()
     return Response(json.dumps(state), status=200, mimetype='application/json')
+
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
